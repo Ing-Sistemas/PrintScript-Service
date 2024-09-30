87de5067de93ae441dd0554f1b7f94edbcf897d6
agussoul <aalma@mail.austral.edu.ar>
2024-09-30 19:56:34 -0300

    added springboot

---
+++ src/main/kotlin/ingsis/printscriptservice/PrintScriptServiceApplication.kt
@@ -0,0 +1,11 @@
+package ingsis.printscriptservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrintScriptServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrintScriptServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ingsis/printscriptservice/PrintScriptServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsis.printscriptservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrintScriptServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
