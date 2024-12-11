8308c4a944c0d53ff26032a1207e2c673b67f7db
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-12-11 16:49:16 -0300

    0.79

--- src/main/kotlin/app/utils/RunTestDto.kt
+++ src/main/kotlin/app/utils/RunTestDto.kt
@@ -5,8 +5,7 @@ data class RunTestDTO (
     val name: String?,
     val input: List<String>,
     val output: List<String>,
-    val status: TestStatus? = null
-)
+    val status: TestStatus? = null)
 
 enum class TestStatus {
     PENDING,
